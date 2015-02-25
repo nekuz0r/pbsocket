c918cf2013040d81d21a756ee72c8cbf07d68271
Leandre Gohy <leandre.gohy@hexeo.be>
2015-02-25 11:55:25 +0100

    Update examples

--- example/tcp_server.js
+++ example/tcp_server.js
@@ -3,10 +3,6 @@ var TCPServer = require('../TCPServer');
 var s = new TCPServer(8000, __dirname + '/example.proto', 5678);
 s.on('listening', function() {
   s.on('connect', function(client) {
-    client.on('message', function(msg) {
-      console.log('message', msg.name, 'from', msg.source, '(', msg.isContentUnserialized ,')')
-    });
-    
     client.on('Hello', function(msg) {
       client.sendTo(msg.source, 'World', { content: 'World' });
     });
